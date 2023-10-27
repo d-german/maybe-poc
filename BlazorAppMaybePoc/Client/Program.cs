b2a20a61f2ec01f585a77aaf8c77c3d28fc8032c
Damon German <damon.german@hyland.com>
2023-10-27 11:00:56 -0500

    Refactor code to use C# file-scoped namespaces
    This commit modernizes the project architecture by refactoring it to use C# file-scoped namespaces instead of block-scoped namespaces. It applies this change consistently across all classes in the Server, Client, and Shared project directories.

--- BlazorAppMaybePoc/Client/Program.cs
+++ BlazorAppMaybePoc/Client/Program.cs
@@ -1,22 +1,21 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace BlazorAppMaybePoc.Client
+namespace BlazorAppMaybePoc.Client;
+
+public static class Program
 {
-    public class Program
+    public static Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped(sp => new HttpClient
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+            BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+        });
 
-            builder.Services.AddScoped(sp => new HttpClient
-            {
-                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
-            });
-
-            await builder.Build().RunAsync();
-        }
+        return builder.Build().RunAsync();
     }
 }
