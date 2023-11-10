d2f427109d25beb647ffcafb8a759be5f3eb125f
Damon German <damon.german@hyland.com>
2023-11-10 14:13:09 -0600

    Add TemperatureConversionServiceMap tests
    A new file, TemperatureConversionServiceMapTests.cs, was created in the BlazorMaybePocTests directory. This file includes tests for the FahrenheitToCelsius and CelsiusToFahrenheit methods, ensuring they provide expected results.

---
+++ BlazorAppMaybePoc/BlazorMaybePocTests/TemperatureConversionServiceMapTests.cs
@@ -0,0 +1,29 @@
+using BlazorAppMaybePoc.Client;
+
+namespace BlazorMaybePocTests;
+
+[TestFixture]
+public class TemperatureConversionServiceMapTests
+{
+    [TestCase(32.0, "0°C")]
+    [TestCase(212.0, "100°C")]
+    [TestCase(0.0, "-17.78°C")]
+    [TestCase(100.0, "37.78°C")]
+    // Add more test cases as needed
+    public void FahrenheitToCelsius_ReturnsExpectedResult(double fahrenheit, string expectedCelsius)
+    {
+        var result = TemperatureConversionServiceMap.FahrenheitToCelsius((decimal)fahrenheit);
+        Assert.That(result, Is.EqualTo(expectedCelsius));
+    }
+
+    [TestCase(0.0, "32°F")]
+    [TestCase(100.0, "212°F")]
+    [TestCase(-17.78, "0.00°F")]
+    [TestCase(37.78, "100.00°F")]
+    // Add more test cases as needed
+    public void CelsiusToFahrenheit_ReturnsExpectedResult(double celsius, string expectedFahrenheit)
+    {
+        var result = TemperatureConversionServiceMap.CelsiusToFahrenheit((decimal)celsius);
+        Assert.That(result, Is.EqualTo(expectedFahrenheit));
+    }
+}
